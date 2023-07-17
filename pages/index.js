600bc7cb4efd3ab273f8b8da27d846caea410a6c
Haarish Anandan <131394736+haarish04@users.noreply.github.com>
2023-07-18 02:20:24 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import VideogameAssetRoundedIcon from '@mui/icons-material/VideogameAssetRounded';
+import ImageSlider from '../components/ImageSlider';
+import { SliderData } from '../components/SliderData';
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.middleButtons}>
-        <VideogameAssetRoundedIcon fontSize="large" sx={{ color: 'white' }} className={styles.icon} />
-        <button className={styles.button}>About</button>
-        <button className={styles.button}>Explore</button>
-        <button className={styles.button}>Wishlist</button>
-        <button className={styles.button}>Cart</button>
+    <>
+      <div className={styles.container}>
+        <div className={styles.middleButtons}>
+          <VideogameAssetRoundedIcon fontSize="large" sx={{ color: 'white' }} className={styles.icon} />
+          <button className={styles.button}>About</button>
+          <button className={styles.button}>Explore</button>
+          <button className={styles.button}>Wishlist</button>
+          <button className={styles.button}>Cart</button>
+        </div>
+        <div className={styles.rightButton}>
+          <loginbtn className={styles.loginbtn}>SIGN IN</loginbtn>
+        </div>
       </div>
-      <div className={styles.rightButton}>
-        <loginbtn className={styles.loginbtn}>SIGN IN</loginbtn>
+      <div className={styles.Slider}>
+        <ImageSlider slides={SliderData} />
       </div>
-    </div>
+    </>
   )
 }
