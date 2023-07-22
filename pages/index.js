58916ee75391b73f718b3754a8f3b5bfcbe3e39f
Haarish Anandan <131394736+haarish04@users.noreply.github.com>
2023-07-23 02:23:34 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,13 @@
 import styles from '@/styles/Home.module.css'
-import VideogameAssetRoundedIcon from '@mui/icons-material/VideogameAssetRounded';
 import ImageSlider from '../components/ImageSlider';
 import { SliderData } from '../components/SliderData';
-
+import TopBar from '@/components/TopBar';
 
 export default function Home() {
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.middleButtons}>
-          <VideogameAssetRoundedIcon fontSize="large" sx={{ color: 'white' }} className={styles.icon} />
-          <button className={styles.button}>About</button>
-          <button className={styles.button}>Explore</button>
-          <button className={styles.button}>Wishlist</button>
-          <button className={styles.button}>Cart</button>
-        </div>
-        <div className={styles.rightButton}>
-          <loginbtn className={styles.loginbtn}>SIGN IN</loginbtn>
-        </div>
+      <div className={styles.TopBar}>
+        <TopBar />
       </div>
       <div className={styles.Slider}>
         <ImageSlider slides={SliderData} />
